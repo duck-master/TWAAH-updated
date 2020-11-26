0e41fd35ade14ac8d4b1a6c2f65eaf5ea0f48d5e
duck-master <57124328+duck-master@users.noreply.github.com>
2020-11-26 02:32:08 -0500

    first true novel draft
    converted functions so that they all output strings, added y2k38 simulator, wrote output into file

--- nanogenmo20-master.py
+++ nanogenmo20-master.py
@@ -68,34 +68,34 @@ class Actor:
     
     def commentObject(self, objects, otherPlayer):
         '''Function for an Actor to comment on objects in the room.
-        self, [str] -> None
-        prints str'''
+        self, [str] -> str'''
+        result = ''
         if(len(objects) > 0):
-            newRoom(self)
-            talkAboutObject(self.name, choice(objects))
+            result += newRoom(self)
+            result += talkAboutObject(self.name, choice(objects))
             if uniform(0, 1) < 0.25:
-                self.think(otherPlayer)
+                result += self.think(otherPlayer)
+        return result
                 
     def action(self, actions, otherPlayer):
         '''Function for an Actor to perform actions.
-        self, [str] -> None
-        prints str'''
+        self, [str] -> str'''
+        result = ''
         if(len(actions) > 0):
-            newRoom(self)
-            print(f'\n{self.name} {choice(actions)}')
+            result += newRoom(self)
+            result += f'\n{self.name} {choice(actions)}'
             if uniform(0, 1) < 0.25:
-                self.think(otherPlayer)
+                result += self.think(otherPlayer)
+        return result
 
     def think(self, otherPlayer):
         '''Function for a Player to reflect.
-        self, Actor -> None
-        prints str'''
+        self, Actor -> str'''
         feelings = ['loved', 'hated', 'envied', 'liked']
         possibleThoughts = [
-            f'She thought about {otherPlayer.name}.',
             f'She {choice(feelings)} her so damn much.',
-            f'She wanted that {choice(otherPlayer.items)}. Nothing else would do',
-            f'It was almost too much to deal with',
+            f'She wanted that {choice(otherPlayer.items)}. Nothing else would do.',
+            f'It was almost too much to deal with.',
             f'Why couldn\'t she be more like her?',
             f'Maybe if they could just be real with each other for once... ',
             f'Nobody really knew how she felt. ',
@@ -103,10 +103,10 @@ class Actor:
             f'Just thinking about it made her want to puke. ',
             f'Why did she come to this stupid house party in the first place?'
         ]
-        thoughts = ''
+        thoughts = f'She thought about {otherPlayer.name}. '
         for pt in possibleThoughts:
-            if uniform(0, 1) < 0.4:
-                thoughts += pt
+            if uniform(0, 1) < 0.3:
+                thoughts += pt + ' '
         #TODO: augment with GPT-2
         return thoughts
     
@@ -133,7 +133,7 @@ for (roomname, roomjson) in thehousejson.items():
 #	''')
 #print("end")
 
-#TODO: initializing names
+#initializing names
 with open('names.txt', mode = 'r') as f:
     nameslines = f.readlines()
 
@@ -170,7 +170,7 @@ def makeItems(num):
 
 players = [Actor(choice(names), "front yard", makeItems(2)) for i in range(6)]
 
-print(', '.join([player.name for player in players]) + ' found themselves dropped off at the same house. How awkward.')
+
 
 #data
 looked = [
@@ -210,84 +210,10 @@ with open('dreams.txt', mode = 'r') as f:
     dreams = list(map((lambda s: s[:-2]), f.readlines()))
 
 #helper functions
-
-#TODO: augment with GPT-2
-def dialogue(actor1, actor2):
-    '''Generates a conversation between two characters.
-    Actor, Actor -> None
-    prints str'''
-    randno = uniform(0, 1)
-    if randno < 0.5:
-        pass
-    elif randno < 0.71:
-        print(f'{actor1.name} and {actor2.name} had a nice conversation.')
-        #in karpathy: calls generate, generateBest, generateFav
-        #TODO: replace with GPT-2 calls
-    elif randno < 0.92:
-        stare(actor1, actor2)
-    else:
-        dream(actor1, actor2)
-        
-
-def dream(actor1, actor2):
-    '''Represents discussion about dreams.
-    Actor, Actor -> None
-    prints str'''
-    pre = choice([
-	actor2.name + ' looked directly at ' + actor1.name + '. "I had the weirdest dream," she said.',
-	actor2.name + ' stiffened and began to ramble, not noticing ' + actor1.name + ', as if in a fugue state.',
-	'"' + actor2.name + ', what did you dream about last night?" asked ' + actor1.name + '. ' + actor2.name + ' seemed wary, but her face softened.',
-	'"Dreams," ' + actor1.name + ' said. "Tell me about your dreams." ' + actor2.name + ' almost said nothing, but thought better of it.',
-	actor2.name + ' was crying. ' + actor1.name + ' acted on instinct. "Tell me all about it." she said. ' + actor2.name + ' obliged in an unbecoming outpouring.',
-    ])
-    encounter(actor1, actor2)
-    print(pre + '\n', end = choice(dreams))
-
-def stare(actor1, actor2):
-    '''Represents two characters uncomfortably staring at one another.
-    Actor, Actor -> None
-    prints str'''
-    print('\n');
-    encounter(actor1, actor2);
-    print(choice([
-        actor1.name + ' stared at ' + actor2.name + ' suspiciously.',
-        actor1.name + ' avoided ' + actor2.name + '.',
-        actor1.name + ' eyed ' + actor2.name + '\'s ' + choice(actor2.items) + ' with envy.',
-        '"Where did you get that ' + choice(actor2.items) + ', ' + actor2.name + '?" ' + actor2.name +' ignored her.',
-        actor1.name + ' played nervously with her ' + choice(actor1.items) + ' in a successful bid to avoid talking to ' + actor2.name + '.'
-    ]))
-    if (uniform(0, 1) < 0.25):
-        actor1.think(actor2)
-
-def solo(actor):
-    '''Describes a character doing stuff in a room alone.
-    Actor -> None
-    prints str'''
-    if(uniform(0, 1) > 0.2):
-        if (uniform(0, 1) < 0.45):
-            actor.commentObject(thehouse.roomof(actor).objects, choice(players))
-        else:
-            actor.action(thehouse.roomof(actor).actions, choice(players))
-
-def newRoom(actor):
-    '''Describing a character entering a room.
-    Actor -> None
-    prints str'''
-    print('\n' + choice([
-        'After some time, ' + actor.name + ' found herself in the ' + actor.loc + '.',
-        actor.name + ' entered the ' + actor.loc + '.',
-        'Nobody was in the ' + actor.loc + ', so ' + actor.name + ' found herself uncharacteristically at ease.',
-        '"Perfect, I\'ve got the ' + actor.loc + ' all to myself," thought ' + actor.name + '.',
-        'The ' + actor.loc + ' was empty. Finally, ' + actor.name + ' thought, somewhere she could think.',
-        actor.name + ' looked around the ' + actor.loc + '.',
-        actor.name + ' breathed deeply and took in sights of the ' + actor.loc + '.'
-      ]))
-
 def encounter(actor1, actor2):
     '''Describes two characters encountering each other.
-    Actor, Actor -> None
-    prints str'''
-    print(choice([
+    Actor, Actor -> str'''
+    return choice([
         actor1.name + ' encountered ' + actor2.name + ' in the ' + actor1.loc + '.',
         actor1.name + ' and ' + actor2.name + ' ran into each other in the ' + actor1.loc + '.',
         actor1.name + ' entered the ' + actor1.loc + '. ' + actor2.name + ' was there, as if waiting.',
@@ -296,13 +222,80 @@ def encounter(actor1, actor2):
         actor1.name + ' entered the ' + actor1.loc + ' to find ' + actor2.name + ' standing there.',
         actor1.name + ' walked into the ' + actor1.loc + ' and saw ' + actor2.name + '. Great.',
         'The ' + actor1.loc + ' held two items of interest to ' + actor1.name + ': the ' + choice(thehouse.roomof(actor1).objects) + ' and ' + actor2.name + '.'
-    ]))
+    ])
+
+
+def dream(actor1, actor2):
+    '''Represents discussion about dreams.
+    Actor, Actor -> str'''
+    pre = choice([
+	actor2.name + ' looked directly at ' + actor1.name + '. "I had the weirdest dream," she said.',
+	actor2.name + ' stiffened and began to ramble, not noticing ' + actor1.name + ', as if in a fugue state.',
+	'"' + actor2.name + ', what did you dream about last night?" asked ' + actor1.name + '. ' + actor2.name + ' seemed wary, but her face softened.',
+	'"Dreams," ' + actor1.name + ' said. "Tell me about your dreams." ' + actor2.name + ' almost said nothing, but thought better of it.',
+	actor2.name + ' was crying. ' + actor1.name + ' acted on instinct. "Tell me all about it." she said. ' + actor2.name + ' obliged in an unbecoming outpouring.',
+    ])
+    return encounter(actor1, actor2) + '\n' + pre + '\n' + choice(dreams)
+
+def stare(actor1, actor2):
+    '''Represents two characters uncomfortably staring at one another.
+    Actor, Actor -> str'''
+    result = '\n' + encounter(actor1, actor2)
+    result += ' ' + choice([
+        actor1.name + ' stared at ' + actor2.name + ' suspiciously.',
+        actor1.name + ' avoided ' + actor2.name + '.',
+        actor1.name + ' eyed ' + actor2.name + '\'s ' + choice(actor2.items) + ' with envy.',
+        '"Where did you get that ' + choice(actor2.items) + ', ' + actor2.name + '?" ' + actor2.name +' ignored her.',
+        actor1.name + ' played nervously with her ' + choice(actor1.items) + ' in a successful bid to avoid talking to ' + actor2.name + '.'
+    ])
+    if (uniform(0, 1) < 0.25):
+        result += ' ' + actor1.think(actor2)
+    return result
+
+#TODO: augment with GPT-2
+def dialogue(actor1, actor2):
+    '''Generates a conversation between two characters.
+    Actor, Actor -> str'''
+    randno = uniform(0, 1)
+    if randno < 0.5:
+        return ''
+    elif randno < 0.71:
+        return f'{actor1.name} and {actor2.name} had a nice conversation.'
+        #in karpathy: calls generate, generateBest, generateFav
+        #TODO: replace with GPT-2 calls
+    elif randno < 0.92:
+        return stare(actor1, actor2)
+    else:
+        return dream(actor1, actor2)
+
+def solo(actor):
+    '''Describes a character doing stuff in a room alone.
+    Actor -> str'''
+    if(uniform(0, 1) > 0.2):
+        if (uniform(0, 1) < 0.45):
+            return actor.commentObject(thehouse.roomof(actor).objects, choice(players))
+        else:
+            return actor.action(thehouse.roomof(actor).actions, choice(players))
+    else:
+        return ''
+
+def newRoom(actor):
+    '''Describing a character entering a room.
+    Actor -> str'''
+    return '\n' + choice([
+        'After some time, ' + actor.name + ' found herself in the ' + actor.loc + '.',
+        actor.name + ' entered the ' + actor.loc + '.',
+        'Nobody was in the ' + actor.loc + ', so ' + actor.name + ' found herself uncharacteristically at ease.',
+        '"Perfect, I\'ve got the ' + actor.loc + ' all to myself," thought ' + actor.name + '.',
+        'The ' + actor.loc + ' was empty. Finally, ' + actor.name + ' thought, somewhere she could think.',
+        actor.name + ' looked around the ' + actor.loc + '.',
+        actor.name + ' breathed deeply and took in sights of the ' + actor.loc + '.'
+      ])
 
 #TODO: augment with GPT-2 calls
 def talkAboutObject(name, obj):
     '''Describes a character talking about an object.
-    str, str -> None
-    prints str'''
+    str, str -> str'''
     talks = [
 	'The ' + obj + ' caught her eye. She thought it was the ugliest thing she\'d ever seen.',
 	'She stared at the ' + obj + ' uncomprehendingly.',
@@ -321,14 +314,13 @@ def talkAboutObject(name, obj):
 	'She wished the booze would kick in.',
 	'She heard a noise from somewhere else in the house.'
     ]
-    print('\n' + choice(talks) + ' ' + choice(posts))
+    return '\n' + choice(talks) + ' ' + choice(posts)
 
 #main code
 def generateVignettes():
     '''The main function.
-    None -> None
-    prints str'''
-    dialogues = []
+    None -> str'''
+    result = ''
     for player in players:
         player.leaveRoom(thehouse.roomof(player))
 
@@ -336,15 +328,29 @@ def generateVignettes():
         othersHere = list(filter((lambda el: el.loc == player.loc and el.name != player.name), players))
         if (len(othersHere) > 0):
           other = choice(othersHere)
-          dialogue(player, other)
+          result += dialogue(player, other)
         else:
-          solo(player)
+          result += solo(player)
+    return result
+
+#minor helper func to simulate y2k38 errors
+def simul2k38(dt):
+	if dt > datetime(2038,1,18,19,14,7): #i'm going to assume the mansion is in PT, because karpathy likes it
+		return dt - timedelta(seconds = 2**32)
+	return dt
 
 #calling the main code
-timestamp = datetime(2038, 1, 18, randrange(4, 6), randrange(60), randrange(60))
+timestamp = datetime(2038, 1, 18, randrange(16, 18), randrange(60), randrange(60))
 vigno = 1
-for i in range(800):
-    print(f'\n-------------\nREPORT {vigno}\n{timestamp} PT\n') #because karpathy likes california, I'll set the mansion in PT
-    generateVignettes()
-    timestamp += timedelta(seconds = randrange(50, 100))
-    vigno += 1
+wordswritten = 0
+with open('twaahu-draft1.txt', mode = 'w') as outf:
+    outf.write(', '.join([player.name for player in players]) + ' found themselves dropped off at the same house. How awkward.')
+    while wordswritten < 51000:
+        outf.write(f'\n-------------\nREPORT {vigno}\n{simul2k38(timestamp)} PT\n') 
+        currentvig = generateVignettes()
+        outf.write(currentvig)
+        timestamp += timedelta(seconds = int(uniform(2/3, 4/3)*len(currentvig.split(' '))))
+        vigno += 1
+        wordswritten += len(currentvig.split(' '))
+        if vigno % 42 == 0:                           #progress bar
+            print(f'PROGRESS: currently writing vignette {vigno}; {51000 - wordswritten} words to go')
